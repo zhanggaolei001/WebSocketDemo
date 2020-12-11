c89a17c2a19397bec6f62af6f4802329f8315038
zhanggaolei <zhanggaolei@qq.com>
2020-12-11 11:21:47 +0800

    add server and client code

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace Client
+{
+    public class ClientProgram
+    {
+        public static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://127.0.0.1:8888/Hello"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Laputa says: " + e.Data);
+
+                ws.Connect();
+                ws.Send("Hello World");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Server
+{
+
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer(8888);
+            wssv.AddWebSocketService<Laputa>("/Hello");
+            wssv.Start();
+            Console.ReadKey(true);
+            while (Console.ReadKey().Key != ConsoleKey.Q)
+            {
+            }
+            wssv.Stop();
+        }
+        class Laputa : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("Got message from client:" + e.Data);
+                Send("Hi,client,I'm the server ,I've got your message:" + e.Data);
+            }
+        }
+    }
+}
